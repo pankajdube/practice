33a307e626c65181e59c127a3f346129f9c30701
Pankaj Dubey <pankaj.dubey@samsung.com>
2015-08-25 17:26:58 +0530

    Updated LCS
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>

--- dynamic_problem/lcs.c
+++ dynamic_problem/lcs.c
@@ -8,5 +8,20 @@ int max(int a, int b)
 int lcs(char* str1, char* str2, int len1, int len2)
 {
 	if (str1[len1] == str2[len2])
-		return
+		return (1 + lcs(str1, str2, len1 -1, len2 -1));
+	else
+		return max(lcs(str1, str2, len1 -1, len2), lcs(str1, str2, len1, len2-1));
+}
+
+int main()
+{
+	int len1, len2, lcs_count;
+	char str1[] = "AGGTAB";
+	char str2[] = "GXTXAYB";
+	len1 = sizeof(str1)/sizeof(char);
+	len2 = sizeof(str2)/sizeof(char);
+	lcs_count = lcs(str1, str2, len1, len2);
+	printf("Lenght of longest common subsequence is: %d\n", lcs_count);
+	return 0;
+
 }
