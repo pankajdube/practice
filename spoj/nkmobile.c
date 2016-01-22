238481e1a9c684c113a519fea039d0ecf88c28f1
Pankaj Dubey <pankaj.dubey@samsung.com>
2016-01-22 19:58:12 +0530

    nkmobile program updated
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>

--- spoj/nkmobile.c
+++ spoj/nkmobile.c
@@ -1,66 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <malloc.h>
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#define lowbit(x)x&(-x)
+using namespace std;
 
-void printvalues(int **data, int size)
-{
-	int i, j;
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++) {
-			printf("%d ", data[i][j]);
-		}
-	printf("\n");
+const int MAXS = 1025;
+int S,cmd,ans;
+int X,Y,A,L,B,R,T;
+int tree[MAXS][MAXS];
+
+void inc(int r,int c,int val){
+    for(int i = r;i <= S;i+=lowbit(i))
+        for(int j = c;j <= S;j+=lowbit(j)) {
+            tree[i][j]+=val;
 	}
 }
 
-long long totalactivephones(int **data, int X1, int X2, int Y1, int Y2)
-{
-	int i, j;
-	long total = 0;
-	for (i = X1; i <= X2; i++) {
-			for (j = Y1; j <= Y2; j++) {
-				total += data[i][j];
-			}
-	}
-
-	return total;
+long long sum(int r,int c){
+    int ret = 0;
+    for(int i = r;i > 0;i-=lowbit(i))
+        for(int j = c;j > 0;j-=lowbit(j))
+            ret+=tree[i][j];
+    return ret;
 }
 
-int main ()
-{
-	int I, S, X, Y, A, L, B, R, T;
-	int **data;
-	int i;
+long long sum(int r1,int c1,int r2,int c2){
+    return sum(r2,c2)-sum(r1-1,c2)-sum(r2,c1-1)+sum(r1-1,c1-1);
+}
 
-	scanf("%d %d\n", &I, &S);
-
-	data = (int **) malloc(S * sizeof(int *));
-	for (i = 0; i < S; i++) {
-		data[i] = (int *) malloc(S * sizeof(int));
-	}
-	
-	scanf("%d", &I);
-
-	while (I != 3) {
-			switch (I) {
-					case 1:
-							scanf("%d %d %d\n", &X, &Y, &A);
-							data[X][Y] += A;
-							//printvalues(data, S);
-							break;
-					case 2:
-							scanf("%d %d %d %d\n", &L, &B, &R, &T);
-							//printvalues(data, S);
-							printf("%lld\n", totalactivephones(data, L, R, B, T));
-							break;
-			}
-			scanf("%d", &I);
-	}
-	
-	for (i = 0; i < S; i++)
-		free(data[i]);
-
-	free(data);
-
-	return 0;	
+int main(){
+    scanf("%d%d",&cmd,&S);
+    while(scanf("%d",&cmd) && cmd != 3){
+        if(cmd == 1){
+            scanf("%d%d%d",&X,&Y,&A);X++,Y++;
+            inc(X,Y,A);
+        }
+        if(cmd == 2){
+            scanf("%d%d%d%d",&L,&B,&R,&T);L++,B++,R++,T++;
+            printf("%lld\n",sum(L,B,R,T));
+        }
+    }
+    return 0;
 }
