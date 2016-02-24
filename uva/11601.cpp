f6019952e9469f949c84da424a311a911cd46400
Pankaj Dubey <pankaj.dubey@samsung.com>
2016-02-24 16:10:06 +0530

    11601: accepted
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>

--- uva/11601.cpp
+++ uva/11601.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
-#include <vector>
+#include <cstring>
+#include <algorithm>
+#define MAXL 205
 
 using namespace std;
 
@@ -11,88 +13,60 @@ struct point
 
 struct rectangle
 {
-
 	struct point LB;
 	struct point RT;
-	bool drawn;
 };
 
-struct rectangle right_most_rect;
+int rectangles[MAXL][MAXL];
 
-vector<struct rectangle> rectangles;
-
-inline unsigned int compute_area(struct rectangle rect)
+inline int compute_area(struct rectangle rect)
 {
 	return ((rect.RT.x - rect.LB.x) * (rect.RT.y - rect.LB.y));
 }
 
 bool is_overlapping(struct rectangle cr)
 {
-	bool overlapping = false;
-	int i;
-	for (i = 0; i < rectangles.size(); i++) {
-		struct rectangle r = rectangles[i];
-		if ((cr.LB.x >= right_most_rect.RT.x || cr.LB.y >= right_most_rect.RT.y))
-			break;
-		
-//		if (!r.drawn)
-//			continue;	
-//		printf("Checking (%d %d %d %d) with (%d %d %d %d)\n", cr.LB.x, cr.LB.y, cr.RT.x, cr.RT.y, r.LB.x, r.LB.y, r.RT.x, r.RT.y);
-		if ((r.LB.x <= cr.LB.x) && (cr.LB.x < r.RT.x) && (r.LB.y <= cr.LB.y) && (cr.LB.y < r.RT.y)) {
-			overlapping = true;
-			break;
+	int i, j;
+	for (i = cr.LB.x; i < cr.RT.x; i++) {
+		for (j = cr.LB.y; j < cr.RT.y; j++) {
+			if (rectangles[i][j])
+				return true;
 		}
-		if ((r.LB.x < cr.RT.x) && (cr.RT.x <= r.RT.x) && (r.LB.y < cr.RT.y) && (cr.RT.y <= r.RT.y)) {
-			overlapping = true;
-			break;
-		}
-		if ((r.LB.x <= cr.LB.x) && (cr.LB.x < r.RT.x) && (cr.LB.y < r.RT.y) && (cr.RT.y > r.LB.y)) {
-			overlapping = true;
-			break;
-		}
-		if ((cr.LB.x <= r.LB.x) && (cr.LB.y <= r.LB.y) && (cr.RT.x >= r.RT.x) && (cr.RT.y >= r.RT.y)) {
-			overlapping = true;
-			break;
-		} 
 	}
-	return overlapping;
+	for (i = cr.LB.x; i < cr.RT.x; i++) {
+		for (j = cr.LB.y; j < cr.RT.y; j++) {
+			rectangles[i][j] = 1;
+		}
+	}
+	return false;
 }
 
 
 int main()
 {
 	int test_cnt, rectangle_cnt;
-	unsigned int area;
-	int i, j;
+	int area;
+	int i, j, k;
 	scanf ("%d\n", &test_cnt);
 	for (i = 1; i <= test_cnt; i++) {
 		area = 0;
-		right_most_rect.LB.x = right_most_rect.LB.y = 0; 
-		/* clear rectangle vector */
-		rectangles.clear();
+		memset(rectangles, 0, sizeof rectangles);
 		/* scan count of rectangles */
 		scanf("%d\n", &rectangle_cnt);
 		/* scan coordinates of rectangles */
 		for (j = 0; j < rectangle_cnt; j++) {
 			struct rectangle r;
 			scanf("%d %d %d %d\n", &r.LB.x, &r.LB.y, &r.RT.x, &r.RT.y);
-			if (right_most_rect.LB.x < r.LB.x)
-				right_most_rect = r;
-			else if (right_most_rect.LB.y < r.LB.y)
-				right_most_rect = r;
-			if (j == 0) {
-				//r.drawn = true;
+			r.LB.x += 100;
+			r.LB.y += 100;
+			r.RT.x += 100;
+			r.RT.y += 100;
+			/* shift  rect by 100 on +x axis and +y axis */
+			if(!is_overlapping(r))
 				area += compute_area(r);
-				rectangles.push_back(r);
-			} else if(!is_overlapping(r)) {
-					//r.drawn = true;
-					area += compute_area(r);
-					rectangles.push_back(r);
-			}
 		}
 	
-		printf("Case %d: %u\n", i, area);
+		printf("Case %d: %d\n", i, area);
 	};
-	printf("\n");	
 	return 0;
 }
