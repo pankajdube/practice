7d0babdb8dd6457a193a3da49d7593a94cd47292
Pankaj Dubey <pankaj.dubey@samsung.com>
2016-03-01 22:36:22 +0530

    WIP: 361
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>

--- uva/361.cpp
+++ uva/361.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 
 #define MAXN 201
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) <= (b) ? (a) : (b))
 
 using namespace std;
 
@@ -25,15 +27,44 @@ inline int area(int x1, int y1, int x2, int y2, int x3, int y3)
 	return abs(x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2));
 }
 
+bool is_inbetween(int x1, int y1, int x2, int y2, int x, int y)
+{
+	int minX, maxX, minY, maxY;
+	maxX = max(x1, x2);
+	minX = (x1 + x2) - maxX;
+	maxY = max(y1, y2);
+	minY = (y1 + y2) - maxY;
+	if (x <= maxX && x >= minX) {
+		if (y <= maxY && y >= minY)
+			return true;
+	}
+	return false;
+}
+
 bool is_inside(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y)
 {
 	int A, A1, A2, A3;
 	
 	A = area(x1, y1, x2, y2, x3, y3);
+	if (A == 0) {
+		if ((x1 == x && y1 == y) || (x2 == x && y2 == y) || (x3 == x && y3 == y))
+			return true;
+	}
+	printf("A: %d Triangle(%d, %d, %d, %d, %d, %d)\n", A, x1, y1, x2, y2, x3, y3);	
 	A1 = area(x, y, x2, y2, x3, y3);
+	if (A1 == 0 && is_inbetween(x2, y2, x3, y3, x, y))
+		return true;
+	printf("A1: %d Triangle(%d, %d, %d, %d, %d, %d)\n", A1, x, y, x2, y2, x3, y3);	
 	A2 = area(x1, y1, x, y, x3, y3);
+	if (A2 == 0 && is_inbetween(x1, y1, x3, y3, x, y))
+		return true;
+	printf("A2: %d Triangle(%d, %d, %d, %d, %d, %d)\n", A2, x1, y1, x, y, x3, y3);	
 	A3 = area(x1, y1, x2, y2, x, y); 
-	//printf("A: %f A1: %f A2: %f A3: %f\n", A, A1, A2, A3);	
+	if (A3 == 0 && is_inbetween(x1, y1, x2, y2, x, y))
+		return true;
+	printf("A3: Triangle(%d: %d, %d, %d, %d, %d, %d)\n", A3, x1, y1, x2, y2, x, y);	
+	if (A == 0 && (A1 == 0 || A2 == 0 || A3 == 0))
+		return true; 
 	return (A == A1 + A2 + A3);
 }
 
